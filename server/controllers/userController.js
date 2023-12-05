7c34004c94c7e5750c4735daddf137f0164758ba
KauanMO <kauaanmatheus@gmail.com>
2023-12-05 11:41:25 -0300

    Inserção de uuid para id de usuário

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,39 +1,30 @@
 const { PrismaClient } = require('@prisma/client')
+const uuid = require('uuid')
 
 const prisma = new PrismaClient()
 
 const checkers = require('../utils/checkers')
 
 module.exports.insertUser = async (req, res) => {
-    const userData = {
+    const insertValidation = validateInsert({
         firstName: req.body.firstName,
         middleName: req.body.middleName,
         lastName: req.body.lastName,
         email: req.body.email,
         password: req.body.password
-    }
+    })
 
-    if (!userData.lastName || !userData.firstName) {
-        res.status(400).send({
-            clientMessage: 'O primeiro e último nome são obrigatórios'
-        })
-    }
-
-    if (!checkers.emailCheck(userData.email)) {
-        res.status(400).send({
-            clientMessage: 'E-mail informado é inválido'
-        })
-    }
-
-    if (!checkers.passwordCheck(userData.password)) {
-        res.status(400).send({
-            clientMessage: 'A senha não coincide com os padrões requisitados'
+    if (insertValidation.status !== 200) {
+        return res.status(insertValidation.status).send({
+            clientMessage: insertValidation.clientMessage,
+            serverMessage: 'Credenciais de cadastro incorretas'
         })
     }
 
     try {
         const newUser = await prisma.user.create({
             data: {
+                userId: uuid.v4(),
                 firstName: userData.firstName,
                 middleName: userData.middleName,
                 lastName: userData.lastName,
@@ -45,15 +36,62 @@ module.exports.insertUser = async (req, res) => {
         res.status(200).send(newUser.userId)
     } catch (e) {
         const errorMessage = getErrorMessageAndStatus(e)
-        res.status(errorMessage.status).send({ serverMessage: errorMessage.serverMessage })
+        res.status(errorMessage.status).send({ clientMessage: errorMessage.clientMessage, serverMessage: errorMessage.serverMessage || e })
     }
 }
 
-function getErrorMessageAndStatus(e) {
+module.exports.findUser = async (req, res) => {
+    try {
+        const userFound = await prisma.user.findUnique({
+            where: {
+                userId: Number(req.params.userId)
+            }
+        })
+
+        res.status(200).send(userFound.firstName)
+    } catch (e) {
+        const errorMessage = getErrorMessageAndStatus(e)
+        res.status(errorMessage.status).send({ clientMessage: errorMessage.clientMessage, serverMessage: errorMessage.serverMessage || e })
+    }
+}
+
+const getErrorMessageAndStatus = e => {
     if (e.code == 'P2002') {
         return {
             status: 409,
-            serverMessage: `Registro ${e.meta.target} duplicado`
+            clientMessage: `Registro ${e.meta.target} duplicado`
         }
+    }
+
+    return {
+        status: 500,
+        clientMessage: `Erro de servidor: ${e}`
+    }
+}
+
+const validateInsert = userData => {
+    if (!userData.lastName || !userData.firstName) {
+        return {
+            status: 400,
+            clientMessage: 'Primeiro e último nome são obrigatórios'
+        }
+    }
+
+    if (!checkers.emailCheck(userData.email)) {
+        return {
+            status: 400,
+            clientMessage: 'E-mail informado é inválido'
+        }
+    }
+
+    if (!checkers.passwordCheck(userData.password)) {
+        return {
+            status: 400,
+            clientMessage: 'A senha não coincide com os padrões requisitados'
+        }
+    }
+
+    return {
+        status: 200
     }
 }
--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,8 @@ const userController = require('./controllers/userController')
 const express = require('express')
 const router = express.Router()
 
-router.post('/user/insertUser', (req, res) => userController.insertUser(req, res))
+router.post('/insertUser', (req, res) => userController.insertUser(req, res))
+router.get('/findUser/:userId', (req, res) => userController.findUser(req, res))
+
 
 module.exports = router
