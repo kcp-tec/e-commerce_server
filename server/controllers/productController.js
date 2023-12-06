cebac48218a8e5c2568762123ac5eaae1602efe1
Uglúk <pedro.silva867@sptech.school>
2023-12-06 15:28:14 -0300

    validação

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,9 +2,9 @@ const { PrismaClient } = require('@prisma/client')
 const uuid = require('uuid')
 const prisma = new PrismaClient()
 const checkers = require('../utils/checkers')
+const errors = require('../utils/errors')
 
 module.exports.insertProduct = async (req, res) => {
-
     const productData = {
         productId: uuid.v4(),
         name: req.body.name,
@@ -15,8 +15,14 @@ module.exports.insertProduct = async (req, res) => {
     }
 
     const insertProductValidation = validateProduct(productData)
+    
+    if(!insertProductValidation.ok) {
+        return res.status(insertProductValidation.status).send({ 
+            clientMessage: insertProductValidation.clientMessage
+        })
+    }
 
-    if(insertProductValidation){
+    try {
         const newProduct = await prisma.product.create({
             data: {
                 productId: uuid.v4(),
@@ -27,9 +33,12 @@ module.exports.insertProduct = async (req, res) => {
                 picture: productData.picture
             }
         })
-    
+
         res.status(200).send({ clientMessage: `Produto ${newProduct.name} cadastrado` })
-    }
+        } catch (e) {
+            const errorMessage = errors.getErrorMessageAndStatus(e)
+            res.status(errorMessage.status).send({ clientMessage: errorMessage.clientMessage, serverMessage: errorMessage.serverMessage || e })
+        }
 }
 
 module.exports.findProductByField = async (req, res) =>{
@@ -50,23 +59,23 @@ module.exports.findProductByField = async (req, res) =>{
     }
 }
 
-const validateProduct = (productData)=>{
-    if(productData.name == null|| productData.category ==null|| productData.price==null){
+const validateProduct = productData => {
+    if(!productData.name || !productData.category || !productData.price){
         return {
-            status:400,
+            status: 400,
             clientMessage: "O nome, produto e preço são obrigatórios"
-        }, false
+        }
     }
 
-    if(productData.price<=0){
+    if(productData.price <= 0){
         return {
-            status:400,
+            status: 400,
             clientMessage:"O preço precisa ser maior que zero"
-        }, false
+        }
     }
 
     return{
-        status:200,
-        ok:true
-    }, true
+        status: 200,
+        ok: true
+    }
 }
