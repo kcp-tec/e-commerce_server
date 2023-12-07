050aa6aad3b1a6e7da5d761de22b04954933be6c
Uglúk <pedro.silva867@sptech.school>
2023-12-07 12:34:04 -0300

    inserção de produto

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,7 @@ router.get('/findUserById/:userId', (req, res) => userController.findUserById(re
 router.put('/blockUser', (req, res) => userController.blockUser(req, res))
 
 // Products
-router.post('/creayeProduct', (req, res) => productController.createProduct(req, res))
+router.post('/createProduct', (req, res) => productController.createProduct(req, res))
 router.get('/findProductByField/:field/:value', (req, res) => productController.findProductByField(req, res))
 router.post(`/insertProduct`, (req, res) => productController.insertProduct(req,res))
 
