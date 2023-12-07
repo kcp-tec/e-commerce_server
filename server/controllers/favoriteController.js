b7d6335b545e520b20464ecde9e48cb07e41a06a
KauanMO <kauaanmatheus@gmail.com>
2023-12-07 13:31:02 -0300

    Desfavoritando

--- server/controllers/favoriteController.js
+++ server/controllers/favoriteController.js
@@ -5,6 +5,22 @@ const errors = require('../utils/errors')
 
 module.exports.favoriteProduct = async (req, res) => {
     try {
+        const favorite = await prisma.favorite.findFirst({
+            where: {
+                AND: [{ productId: req.body.productId }, { userId: req.body.userId }]
+            }
+        })
+
+        if (favorite) {
+            await prisma.favorite.delete({
+                where: {
+                    favoriteId: favorite.favoriteId
+                }
+            })
+
+            return res.status(200).send({ clientMessage: 'Produto desfavoritado' })
+        }
+
         await prisma.favorite.create({
             data: {
                 favoriteId: uuid.v4(),
@@ -15,6 +31,7 @@ module.exports.favoriteProduct = async (req, res) => {
 
         res.status(200).send({ clientMessage: 'Produto favoritado' })
     } catch (e) {
+        console.log(e)
         const errorMessage = errors.getErrorMessageAndStatus(e)
         res.status(errorMessage.status).send({ clientMessage: errorMessage.clientMessage, serverMessage: errorMessage.serverMessage || e })
     }
