b7d92ba1672fb36b30ba094e42eef1ec5b5980f6
KauanMO <kauaanmatheus@gmail.com>
2023-12-07 11:18:06 -0300

    Valor total do carrinho

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -6,12 +6,19 @@ const errors = require('../utils/errors')
 
 module.exports.insertCart = async (req, res) => {
     try {
+        const product = await prisma.product.findUnique({
+            where: {
+                productId: req.body.productId
+            }
+        })
+
         await prisma.cart.create({
             data: {
                 cartId: uuid.v4(),
                 amount: req.body.amount,
-                productProductId: req.body.productId,
-                userUserId: req.body.userId
+                productId: req.body.productId,
+                userId: req.body.userId,
+                totalValue: (product.price * req.body.amount)
             }
         })
 
@@ -26,11 +33,13 @@ module.exports.findCartsByUser = async (req, res) => {
     try {
         const cartsFound = await prisma.cart.findMany({
             where: {
-                userUserId: req.params.userId
+                userId: req.params.userId
             }
         })
 
-        res.status(200).send(cartsFound)
+        cartsFound
+            ? res.status(200).send(cartsFound)
+            : res.status(404).send({ clientMessage: 'Sem carrinhos encontrados' })
     } catch (e) {
         const errorMessage = errors.getErrorMessageAndStatus(e)
         res.status(errorMessage.status).send({ clientMessage: errorMessage.clientMessage, serverMessage: errorMessage.serverMessage || e })
